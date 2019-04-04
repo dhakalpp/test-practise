5940208ebdd51d760bab4c1b6e85cedc80c13064
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-04-04 13:29:16 +1300

    test practise

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question 1");
+            Console.WriteLine("Please enter a letter");
+            string input = Console.ReadLine();
+            switch (input)
+
+            {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                Console.WriteLine($"{input} is a vowel");
+                break;
+                default:
+                Console.WriteLine($"{input} is a consonent");
+                break;
+              
+                 
+            }
+
+            {
+                Console.WriteLine("press enter to exit");
+                Console.ReadLine();
+            }
+
+        }
+
+       
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question 2");
+            Console.WriteLine("please calculate your BMI");
+            Console.WriteLine("what is your weight in kg's: ");
+            float weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("can you please enter your heigh in CM'S: ");
+            float height = float.Parse(Console.ReadLine())/100;
+            Console.WriteLine("your BMI is: " +(weight / (height*height))) ;
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
